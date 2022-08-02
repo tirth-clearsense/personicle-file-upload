596352e03bad1eeb611c7bb8d7bd87a32d2dcbe5
tirth-clearsense <tpatel@clearsense.com>
2022-08-02 11:14:59 -0700

    update

--- app/controllers/user_images_controller.rb
+++ app/controllers/user_images_controller.rb
@@ -47,7 +47,7 @@ class UserImagesController < ApplicationController
       'b',
       'r',
       '',
-      (Time.now + 8*60).utc.iso8601,
+      (Time.now + 9*60).utc.iso8601,
       ''
       # '2021-08-06'
     )
